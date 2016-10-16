d5d6deff84db392473f7b0b15bda45cbf5fbf0bf
Feldrise <admin@feldrise.com>
2016-10-16 17:20:39 +0200

    Add possibilty to me to show text at start of Sielo

--- includes/SStarter.hpp
+++ includes/SStarter.hpp
@@ -1,8 +1,30 @@
 #pragma once
 
 #include <QWidget>
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QGroupBox>
+#include <QLabel>
+#include <QDialogButtonBox>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+
+class TextToShow : public QDialog
+{
+public:
+    TextToShow(QWidget *parent = nullptr);
+    ~TextToShow();
+
+private:
+    QVBoxLayout *m_layout{ new QVBoxLayout(this) };
+    QGroupBox *m_box{ new QGroupBox(this) };
+    QVBoxLayout *m_boxLayout{ new QVBoxLayout(m_box) };
+
+    QLabel *m_textToShow{ new QLabel("Texte", this)};
+    QDialogButtonBox *m_boxBtn{ new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, Qt::Horizontal, this) };
+
+    QNetworkReply *m_reply{};
+};
 
 class SStarter : public QObject
 {
@@ -13,5 +35,6 @@ public:
 private:
     QNetworkReply *m_reply{};
     QString m_currentVersion{ "0.1b\n" };
-    QString m_version{};
+    QString m_version{ "0.1b\n"};
 };
+
--- src/SStarter.cpp
+++ src/SStarter.cpp
@@ -20,10 +20,52 @@ SStarter::SStarter(QObject *parent) :
     }
     else {
         QMessageBox::information(nullptr, "DEBUG", "Sielo est à joure !");
+        m_reply = manager.get(QNetworkRequest(QUrl("http://feldrise.com/Sielo/showTxt.txt")));
+
+        QEventLoop loop2;
+        connect(m_reply, &QNetworkReply::finished, &loop2, &QEventLoop::quit);
+        loop2.exec();
+
+        QString showTxt{ m_reply->readAll() };
+        if(showTxt == "true\n") {
+            QMessageBox::information(nullptr, "DEBUG", "Need to show txt !");
+            TextToShow *textToShow{ new TextToShow() };
+            textToShow->show();
+        }
+        else {
+            QMessageBox::information(nullptr, "DEBUG", "Don't need to show txt !");
+        }
     }
 }
 
 SStarter::~SStarter()
+{
+
+}
+
+TextToShow::TextToShow(QWidget *parent) :
+    QDialog(parent)
+{
+    setModal(true);
+
+    QNetworkAccessManager manager{};
+    m_reply = manager.get(QNetworkRequest(QUrl("http://feldrise.com/Sielo/textToShow.html")));
+
+    QEventLoop loop;
+    connect(m_reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    m_textToShow->setText(m_reply->readAll());
+
+    m_boxLayout->addWidget(m_textToShow);
+    m_layout->addWidget(m_box);
+    m_layout->addWidget(m_boxBtn);
+
+    connect(m_boxBtn, &QDialogButtonBox::accepted, this, &TextToShow::accept);
+    connect(m_boxBtn, &QDialogButtonBox::rejected, this, &TextToShow::close);
+}
+
+TextToShow::~TextToShow()
 {
 
 }
