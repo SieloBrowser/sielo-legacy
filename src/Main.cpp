6d0cd2fd6e18a86153fc7f5241465e7403fa87d1
Feldrise <admin@feldrise.com>
2016-10-19 17:14:19 +0200

    Add translation

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,7 @@
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
 #include <QTextCodec>
 #include <QtWidgets>
 #include <QtWebEngineWidgets>
@@ -13,7 +16,16 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-//	QMessageBox::information(nullptr, "DEBUG", SMainWindow::dataPath);
+//	QMessageBox::information(nulthislptr, "DEBUG", SMainWindow::dataPath);
+
+	// crée un objet "langue française" et l'affecte a la place de l'objet par défaut du système
+	QLocale france(QLocale::French, QLocale::France);
+	QString locale = france.name();
+	QTranslator translator;
+	QMessageBox::information(nullptr, "DEBUG", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	app.installTranslator(&translator);
+
     SStarter *starter{ new SStarter };
     delete starter;
 
