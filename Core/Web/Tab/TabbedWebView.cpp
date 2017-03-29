8dd01371ddfc0212053505b9e89ecdcca3ce2187
Feldrise <admin@feldrise.com>
2017-03-29 22:19:07 +0200

    Core/Web->Tab: [add] TabbedWebView class

---
+++ Core/Web/Tab/TabbedWebView.cpp
@@ -0,0 +1,176 @@
+/***********************************************************************************
+** MIT License                                                                    **
+**                                                                                **
+** Copyright (c) 2017 Victor DENIS (victordenis01@gmail.com)                      **
+**                                                                                **
+** Permission is hereby granted, free of charge, to any person obtaining a copy   **
+** of this software and associated documentation files (the "Software"), to deal  **
+** in the Software without restriction, including without limitation the rights   **
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      **
+** copies of the Software, and to permit persons to whom the Software is          **
+** furnished to do so, subject to the following conditions:                       **
+**                                                                                **
+** The above copyright notice and this permission notice shall be included in all **
+** copies or substantial portions of the Software.                                **
+**                                                                                **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     **
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       **
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+** SOFTWARE.                                                                      **
+***********************************************************************************/
+
+#include "Web/Tab/TabbedWebView.hpp"
+
+#include <QStatusBar>
+
+#include "BrowserWindow.hpp"
+
+#include "History/HistoryManager.hpp"
+
+#include "Web/WebPage.hpp"
+#include "Web/LoadRequest.hpp"
+#include "Web/WebHitTestResult.hpp"
+#include "Web/Tab/WebTab.hpp"
+
+namespace Sn {
+TabbedWebView::TabbedWebView(WebTab* tab) :
+	WebView(tab),
+	m_window(nullptr),
+	m_webTab(tab),
+	m_menu(new QMenu(this))
+{
+	connect(this, &TabbedWebView::loadStarted, this, &TabbedWebView::sLoadStarted);
+	connect(this, &TabbedWebView::loadProgress, this, &TabbedWebView::sLoadProgress);
+	connect(this, &TabbedWebView::loadFinished, this, &TabbedWebView::sLoadFinished);
+	connect(this, &TabbedWebView::urlChanged, this, &TabbedWebView::urlChanged);
+}
+
+void TabbedWebView::setWebPage(WebPage* page)
+{
+	page->setParent(this);
+	setPage(page);
+
+	connect(page, &WebPage::linkHovered, this, &TabbedWebView::linkHovered);
+}
+
+void TabbedWebView::setBrowserWindow(BrowserWindow* window)
+{
+	m_window = window;
+}
+
+int TabbedWebView::tabIndex() const
+{
+	return m_webTab->tabIndex();
+}
+
+QWidget* TabbedWebView::overlayWidget()
+{
+	return m_webTab;
+}
+
+void TabbedWebView::closeView()
+{
+	emit wantsCloseTab(tabIndex());
+}
+
+void TabbedWebView::loadInNewTab(const LoadRequest& request, Application::NewTabType tabType)
+{
+	//TODO: do
+}
+
+bool TabbedWebView::isFullScreen()
+{
+	return (m_window && m_window->isFullScreen());
+}
+
+void TabbedWebView::requestFullScreen(bool enable)
+{
+	if (!m_window)
+		return;
+
+	if (enable)
+		m_window->enterHtmlFullScreen();
+	else
+		m_window->showNormal();
+}
+
+void TabbedWebView::setAsCurrentTab()
+{
+	//TODO: do
+}
+
+void TabbedWebView::userLoadAction(const LoadRequest& request)
+{
+	load(request);
+}
+
+void TabbedWebView::sLoadStarted()
+{
+	m_currentIp.clear();
+}
+
+void TabbedWebView::sLoadProgress(int progress)
+{
+	Q_UNUSED(progress)
+}
+
+void TabbedWebView::sLoadFinished()
+{
+	QHostInfo::lookupHost(url().host(), this, SLOT(setIp(QHostInfo)));
+}
+
+void TabbedWebView::urlChanged(const QUrl& url)
+{
+	if (m_webTab->isCurrentTab() && url.isValid()) {
+		HistoryManager* manager = Application::instance()->historyManager();
+		manager->addHistoryEntry(url.toString());
+	}
+}
+
+void TabbedWebView::linkHovered(const QString& link)
+{
+	if (m_webTab->isCurrentTab() && m_window) {
+		if (link.isEmpty())
+			m_window->statusBar()->clearMessage();
+		else
+			m_window->statusBar()->showMessage(link);
+	}
+}
+
+void TabbedWebView::setIp(const QHostInfo& info)
+{
+	if (info.addresses().isEmpty())
+		return;
+
+	m_currentIp = QString("%1 (%2)").arg(info.hostName(), info.addresses()[0].toString());
+
+	if (m_webTab->isCurrentTab())
+		emit ipChanged(m_currentIp);
+}
+
+void TabbedWebView::newContextMenuEvent(QContextMenuEvent* event)
+{
+	m_menu->clear();
+
+	WebHitTestResult hitTest{page()->hitTestContent(event->pos())};
+	createContextMenu(m_menu, hitTest);
+
+	if (!m_menu->isEmpty()) {
+		const QPoint pos{event->globalPos()};
+		QPoint p{pos.x(), pos.y() + 1};
+
+		m_menu->popup(p);
+		return;
+	}
+
+	WebView::newContextMenuEvent(event);
+}
+
+void TabbedWebView::newMouseMoveEvent(QMouseEvent* event)
+{
+	WebView::newMouseMoveEvent(event);
+}
+}
---
+++ Core/Web/Tab/TabbedWebView.hpp
@@ -0,0 +1,98 @@
+/***********************************************************************************
+** MIT License                                                                    **
+**                                                                                **
+** Copyright (c) 2017 Victor DENIS (victordenis01@gmail.com)                      **
+**                                                                                **
+** Permission is hereby granted, free of charge, to any person obtaining a copy   **
+** of this software and associated documentation files (the "Software"), to deal  **
+** in the Software without restriction, including without limitation the rights   **
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      **
+** copies of the Software, and to permit persons to whom the Software is          **
+** furnished to do so, subject to the following conditions:                       **
+**                                                                                **
+** The above copyright notice and this permission notice shall be included in all **
+** copies or substantial portions of the Software.                                **
+**                                                                                **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     **
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       **
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+** SOFTWARE.                                                                      **
+***********************************************************************************/
+
+#pragma once
+#ifndef SIELOBROWSER_TABBEDWEBVIEW_HPP
+#define SIELOBROWSER_TABBEDWEBVIEW_HPP
+
+#include <QLabel>
+#include <QMenu>
+
+#include <QHostInfo>
+
+#include "Application.hpp"
+
+#include "Web/WebView.hpp"
+
+namespace Sn {
+class WebPage;
+class BrowserWindow;
+class LoadRequest;
+
+class WebTab;
+
+class TabbedWebView: public WebView {
+Q_OBJECT
+
+public:
+	TabbedWebView(WebTab* tab);
+
+	void setWebPage(WebPage* page);
+
+	BrowserWindow* browserWindow() const { return m_window; }
+	void setBrowserWindow(BrowserWindow* window);
+
+	WebTab* webTab() const { return m_webTab; }
+
+	QString getIp() const { return m_currentIp; }
+
+	int tabIndex() const;
+
+	QWidget* overlayWidget() Q_DECL_OVERRIDE;
+	void closeView() Q_DECL_OVERRIDE;
+	void loadInNewTab(const LoadRequest& request, Application::NewTabType tabType) Q_DECL_OVERRIDE;
+
+	bool isFullScreen() Q_DECL_OVERRIDE;
+	void requestFullScreen(bool enable) Q_DECL_OVERRIDE;
+
+signals:
+	void wantsCloseTab(int);
+	void ipChanged(QString);
+
+public slots:
+	void setAsCurrentTab();
+	void userLoadAction(const LoadRequest& request);
+
+private slots:
+	void sLoadStarted();
+	void sLoadProgress(int progress);
+	void sLoadFinished();
+	void urlChanged(const QUrl& url);
+	void linkHovered(const QString& link);
+	void setIp(const QHostInfo& info);
+
+private:
+	void newContextMenuEvent(QContextMenuEvent* event) Q_DECL_OVERRIDE;
+	void newMouseMoveEvent(QMouseEvent* event) Q_DECL_OVERRIDE;
+
+	BrowserWindow* m_window{nullptr};
+	WebTab* m_webTab{nullptr};
+	QMenu* m_menu{nullptr};
+
+	QString m_currentIp{};
+};
+
+}
+
+#endif //SIELOBROWSER_TABBEDWEBVIEW_HPP
