b1ff73335162ce3d6caadf52888048870785dd89
Feldrise <admin@feldrise.com>
2017-04-03 19:06:40 +0200

    Core/Widgets->Tab: [Add] AddTabButton class

---
+++ Core/Widgets/Tab/AddTabButton.hpp
@@ -0,0 +1,56 @@
+/***********************************************************************************
+** MIT License                                                                    **
+**                                                                                **
+** Copyright (c) 2017 Victor DENIS (victordenis01@gmail.com)                      **
+**                                                                                **
+** Permission is hereby granted, free of charge, to any person obtaining a copy   **
+** of this software and associated documentation files (the "Software"), to deal  **
+** in the Software without restriction, including without limitation the rights   **
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      **
+** copies of the Software, and to permit persons to whom the Software is          **
+** furnished to do so, subject to the following conditions:                       **
+**                                                                                **
+** The above copyright notice and this permission notice shall be included in all **
+** copies or substantial portions of the Software.                                **
+**                                                                                **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     **
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       **
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+** SOFTWARE.                                                                      **
+***********************************************************************************/
+
+#pragma once
+#ifndef SIELOBROWSER_ADDTABBUTTON_HPP
+#define SIELOBROWSER_ADDTABBUTTON_HPP
+
+#include <QWheelEvent>
+#include <QMouseEvent>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+
+#include "Utils/ToolButton.hpp"
+
+namespace Sn {
+class MainTabBar;
+class TabWidget;
+
+class AddTabButton: public ToolButton {
+public:
+	AddTabButton(TabWidget* tabWidget, MainTabBar* tabBar);
+
+private:
+	void wheelEvent(QWheelEvent* event);
+	void mouseReleaseEvent(QMouseEvent* event);
+	void dragEnterEvent(QDragEnterEvent* event);
+	void dropEvent(QDropEvent* event);
+
+	MainTabBar* m_tabBar{nullptr};
+	TabWidget* m_tabWidget{nullptr};
+};
+
+}
+
+#endif //SIELOBROWSER_ADDTABBUTTON_HPP
