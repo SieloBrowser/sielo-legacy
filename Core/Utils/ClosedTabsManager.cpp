9d250ca92b9413d87d41b47aa31e3644c08093f8
Feldrise <admin@feldrise.com>
2018-03-11 12:19:16 +0000

    [Update] copyrights

--- Core/Utils/ClosedTabsManager.cpp
+++ Core/Utils/ClosedTabsManager.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************************
 ** MIT License                                                                    **
 **                                                                                **
-** Copyright (c) 2017 Victor DENIS (victordenis01@gmail.com)                      **
+** Copyright (c) 2018 Victor DENIS (victordenis01@gmail.com)                      **
 **                                                                                **
 ** Permission is hereby granted, free of charge, to any person obtaining a copy   **
 ** of this software and associated documentation files (the "Software"), to deal  **
