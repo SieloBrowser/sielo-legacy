1a028b99dfaa4ca56f2bbf2f963b6cea81907715
Feldrise <admin@feldrise.com>
2018-04-08 23:35:10 +0000

    [Update] Version for ArchLinux package

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -71,7 +71,7 @@
 
 namespace Sn {
 
-QString Application::currentVersion = QString("1.10.06b");
+QString Application::currentVersion = QString("1.11.00b");
 
 // Static member
 QList<QString> Application::paths()
@@ -108,7 +108,7 @@ Application::Application(int& argc, char** argv) :
 	// Setting up settings environment
 	QCoreApplication::setOrganizationName(QLatin1String("Feldrise"));
 	QCoreApplication::setApplicationName(QLatin1String("Sielo"));
-	QCoreApplication::setApplicationVersion(QLatin1String("1.10.06"));
+	QCoreApplication::setApplicationVersion(QLatin1String("1.11.00"));
 
 	// QSQLITE database plugin is required
 	if (!QSqlDatabase::isDriverAvailable(QStringLiteral("QSQLITE"))) {
