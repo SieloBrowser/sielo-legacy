823095af599ba6ab14fe6853aa6592ed77e0a08d
Feldrise <admin@feldrise.com>
2018-05-16 23:40:28 +0200

    [Add] SiteInfoWidget for address bar

---
+++ Core/Widgets/SiteInfoWidget.cpp
@@ -0,0 +1,72 @@
+/***********************************************************************************
+** MIT License                                                                    **
+**                                                                                **
+** Copyright (c) 2018 Victor DENIS (victordenis01@gmail.com)                      **
+**                                                                                **
+** Permission is hereby granted, free of charge, to any person obtaining a copy   **
+** of this software and associated documentation files (the "Software"), to deal  **
+** in the Software without restriction, including without limitation the rights   **
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      **
+** copies of the Software, and to permit persons to whom the Software is          **
+** furnished to do so, subject to the following conditions:                       **
+**                                                                                **
+** The above copyright notice and this permission notice shall be included in all **
+** copies or substantial portions of the Software.                                **
+**                                                                                **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     **
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       **
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+** SOFTWARE.                                                                      **
+***********************************************************************************/
+
+#include "SiteInfoWidget.hpp"
+
+#include "Web/Tab/TabbedWebView.hpp"
+
+#include "BrowserWindow.hpp"
+
+namespace Sn {
+
+SiteInfoWidget::SiteInfoWidget(Sn::BrowserWindow* window, QWidget* parent) :
+		LocationBarPopup(parent),
+		m_window(window)
+{
+	setAttribute(Qt::WA_DeleteOnClose);
+	setupUI();
+	setPopupAlignment(Qt::AlignLeft);
+
+	WebView* view = m_window->webView();
+
+	if (view->url().scheme() == QLatin1String("https"))
+		m_secure->setText(tr("Your connection to this site is <b>secured</b>."));
+	else
+		m_secure->setText(tr("Your connection to this site is <b>unsecured</b>."));
+
+	//TODO: Count number of visite (waiting for new database based history system
+
+}
+
+SiteInfoWidget::~SiteInfoWidget()
+{
+	// Empty
+}
+
+void SiteInfoWidget::setupUI()
+{
+	m_layout = new QGridLayout(this);
+
+	m_history = new QLabel(this);
+	m_secure = new QLabel(this);
+	m_moreButton = new QPushButton(tr("More..."), this);
+	m_spacer = new QSpacerItem(10, 10, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+	m_layout->addWidget(m_secure, 0, 0, 1, 2);
+	m_layout->addWidget(m_history, 1, 0, 1, 2);
+	m_layout->addWidget(m_moreButton, 2, 0, 1, 1);
+	m_layout->addItem(m_spacer, 2, 1, 1, 1);
+}
+
+}
---
+++ Core/Widgets/SiteInfoWidget.hpp
@@ -0,0 +1,65 @@
+/***********************************************************************************
+** MIT License                                                                    **
+**                                                                                **
+** Copyright (c) 2018 Victor DENIS (victordenis01@gmail.com)                      **
+**                                                                                **
+** Permission is hereby granted, free of charge, to any person obtaining a copy   **
+** of this software and associated documentation files (the "Software"), to deal  **
+** in the Software without restriction, including without limitation the rights   **
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      **
+** copies of the Software, and to permit persons to whom the Software is          **
+** furnished to do so, subject to the following conditions:                       **
+**                                                                                **
+** The above copyright notice and this permission notice shall be included in all **
+** copies or substantial portions of the Software.                                **
+**                                                                                **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     **
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       **
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+** SOFTWARE.                                                                      **
+***********************************************************************************/
+
+#pragma once
+#ifndef SIELOBROWSER_SITEINFOWIDGET_HPP
+#define SIELOBROWSER_SITEINFOWIDGET_HPP
+
+#include <QWidget>
+
+#include <QGridLayout>
+
+#include <QLabel>
+#include <QPushButton>
+#include <QSpacerItem>
+
+#include "Widgets/LocationBarPopup.hpp"
+
+namespace Sn {
+class BrowserWindow;
+
+class SiteInfoWidget : public LocationBarPopup {
+Q_OBJECT
+
+public:
+	SiteInfoWidget(BrowserWindow* window, QWidget* parent = nullptr);
+	~SiteInfoWidget();
+
+private:
+	void setupUI();
+
+	BrowserWindow* m_window{nullptr};
+
+	// UI
+	QGridLayout* m_layout{nullptr};
+	QLabel* m_history{nullptr};
+	QLabel* m_secure{nullptr};
+	QPushButton* m_moreButton{nullptr};
+	QSpacerItem* m_spacer{nullptr};
+
+};
+}
+
+
+#endif //SIELOBROWSER_SITEINFOWIDGET_HPP
