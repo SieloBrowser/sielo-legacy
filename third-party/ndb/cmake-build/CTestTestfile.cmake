f19ecdce2c3ce1564d4bf748a3e9426aa47c6c90
Feldrise <admin@feldrise.com>
2018-06-06 16:05:45 +0200

    [Remove] CMake build

--- third-party/ndb/cmake-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Cpp/Projects/CLion/sielo-browser/third-party/ndb
-# Build directory: D:/Cpp/Projects/CLion/sielo-browser/third-party/ndb/cmake-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
