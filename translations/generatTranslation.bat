f7f4d8f79e2fc8a9cc13b10ff09be622e3e1c7d7
Feldrise <admin@feldrise.com>
2018-06-20 11:15:23 +0200

    [Fix] Translation script

--- translations/generatTranslation.bat
+++ translations/generatTranslation.bat
@@ -1,1 +1,1 @@
-lupdate ..\Core -ts "fr_FR.ts" -no-obsolete && lupdate ..\Core -ts "de_DE.ts" -no-obsolete && lupdate ..\Core -ts "en_ES.ts" -no-obsolete && lupdate ..\Core -ts "to-translate.ts" -no-obsolete && lupdate ..\Core -ts "ar_AR.ts" -no-obsolete
+lupdate ..\Core -ts "fr_FR.ts" -no-obsolete && lupdate ..\Core -ts "de_DE.ts" -no-obsolete && lupdate ..\Core -ts "es_ES.ts" -no-obsolete && lupdate ..\Core -ts "ar_AR.ts" -no-obsolete && lupdate ..\Core -ts "to-translate.ts" -no-obsolete
