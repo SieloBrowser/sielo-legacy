46d65c2a7f21e0884220378f94276ed1920278a6
Feldrise <admin@feldrise.com>
2018-06-29 18:31:30 +0200

    [Add] ColorZilla

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -454,7 +454,7 @@ void Application::loadThemesSettings()
 	// Check if the theme existe
 	if (themeInfo.exists()) {
 		// Check default theme version and update it if needed
-		if (settings.value("Themes/defaultThemeVersion", 1).toInt() < 39) {
+		if (settings.value("Themes/defaultThemeVersion", 1).toInt() < 40) {
 			if (settings.value("Themes/defaultThemeVersion", 1).toInt() < 11) {
 				QString defaultThemePath{paths()[Application::P_Themes]};
 
@@ -478,7 +478,8 @@ void Application::loadThemesSettings()
 			loadThemeFromResources("firefox-like-dark", false);
 			loadThemeFromResources("sielo-flat", false);
 			loadThemeFromResources("sielo-default", false);
-			settings.setValue("Themes/defaultThemeVersion", 39);
+			loadThemeFromResources("ColorZilla", false);
+			settings.setValue("Themes/defaultThemeVersion", 40);
 		}
 
 		loadTheme(settings.value("Themes/currentTheme", QLatin1String("sielo-default")).toString(),
@@ -489,8 +490,9 @@ void Application::loadThemesSettings()
 		loadThemeFromResources("firefox-like-light", false);
 		loadThemeFromResources("firefox-like-dark", false);
 		loadThemeFromResources("sielo-flat", false);
+		loadThemeFromResources("ColorZilla", false);
 		loadThemeFromResources();
-		settings.setValue("Themes/defaultThemeVersion", 39);
+		settings.setValue("Themes/defaultThemeVersion", 40);
 	}
 }
 
