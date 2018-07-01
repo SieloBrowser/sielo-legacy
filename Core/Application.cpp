92a34a1ff3a0d4d0ce7d34d9c8b72547a3e0351c
Feldrise <admin@feldrise.com>
2018-07-01 21:58:34 +0200

    [Update] Version

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -83,7 +83,7 @@
 
 namespace Sn
 {
-QString Application::currentVersion = QString("1.16.06");
+QString Application::currentVersion = QString("1.16.07");
 
 // Static member
 QList<QString> Application::paths()
@@ -175,7 +175,7 @@ Application::Application(int& argc, char** argv) :
 	// Setting up settings environment
 	QCoreApplication::setOrganizationName(QLatin1String("Feldrise"));
 	QCoreApplication::setApplicationName(QLatin1String("Sielo"));
-	QCoreApplication::setApplicationVersion(QLatin1String("1.16.06"));
+	QCoreApplication::setApplicationVersion(QLatin1String("1.16.07"));
 
 	setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
 	/*
