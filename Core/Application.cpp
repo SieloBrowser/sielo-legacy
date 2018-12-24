32e2e63ef5b1d01c9876edb6c8032072296d54e7
Feldrise <admin@feldrise.com>
2018-12-24 16:41:47 +0100

    [Update] Version

--- Core/Application.cpp
+++ Core/Application.cpp
@@ -89,7 +89,7 @@
 
 namespace Sn
 {
-QString Application::currentVersion = QString("1.17.11");
+QString Application::currentVersion = QString("1.17.12");
 
 // Static member
 Application *Application::instance()
@@ -224,7 +224,7 @@ Application::Application(int& argc, char** argv) :
 {
 	// Setting up settings environment
 	QCoreApplication::setApplicationName(QLatin1String("Sielo"));
-	QCoreApplication::setApplicationVersion(QLatin1String("1.17.11"));
+	QCoreApplication::setApplicationVersion(QLatin1String("1.17.12"));
 
 	setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
 	setAttribute(Qt::AA_EnableHighDpiScaling);
